a48dbd9a70577da95001eccc37d7c9377f0bb2b0
Link Strifer <linkstrifer@outlook.com>
2022-07-26 14:31:51 -0700

    add UI results

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,46 @@
+import { useState } from "react";
 import { InputFile } from "./components/InputFile";
 import "./styles.css";
 import { parseCSV } from "./utils/csv";
 
 export default function App() {
+  const [data, setData] = useState<any[]>([]);
   return (
     <div className="App">
       <h1>CSV Reader</h1>
       <InputFile
         onLoadFile={(file) => {
-          parseCSV(file).then(console.log);
+          parseCSV(file).then((result) => setData(result));
         }}
       />
+
+      {data.map((row, index) => {
+        const data = Object.getOwnPropertyNames(row);
+
+        return (
+          <details
+            key={index}
+            style={{
+              alignItems: "flex-start",
+              border: "1px solid rgba(0,0,0,.3)",
+              display: "flex",
+              flexDirection: "column",
+              margin: "1em",
+              padding: "1em",
+              textAlign: "left"
+            }}
+          >
+            <summary>
+              <strong>{data[0]}:</strong> {row[data[0]]}
+            </summary>
+            {data.slice(1).map((header) => (
+              <div key={header}>
+                <strong>{header}</strong>: {row[header]}
+              </div>
+            ))}
+          </details>
+        );
+      })}
     </div>
   );
 }
